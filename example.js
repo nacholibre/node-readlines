19c47fa6b34c58c9dec9591c10ebd83343cd2032
Yoan Arnaudov <jonidev@gmail.com>
2015-01-16 15:38:25 +0200

    update example

--- example.js
+++ example.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var lineByLine = require('./readlines.js');
-
 var liner = new lineByLine('./dummy_files/twoLineFile.txt');
 
 var line;
+var lineNumber = 0;
 while (line = liner.next()) {
-    console.log(line.toString('ascii'));
+    console.log('Line ' + lineNumber + ': ' + line.toString('ascii'));
+    lineNumber++;
 }
 
 console.log('end of line reached');
